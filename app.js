f4b0449a58d4d7d450f839659ae14aad76c4f2a3
muhammadarman0 <armandevs74@gmail.com>
2025-11-21 15:35:24 +0500

    Not complete this Assignment

---
+++ app.js
@@ -0,0 +1,95 @@
+// Question no 1
+
+// function currentFatw() {
+//     let currentDate = new Date();
+
+//     console.log(currentDate);
+// }
+
+// currentFatw()
+
+// Question no 2
+
+// function firstAndlastName() {
+//     let firstName = "Muhammad";
+
+//     let lastName = " Arman"
+
+//     let concat = firstName + lastName;
+
+//     console.log("User name: ",concat);
+// }
+
+// firstAndlastName()
+
+// Question no 3
+
+// function operator(){
+//     let num1 = +prompt("Enter first number")
+//     let num2 = +prompt("Enter second number")
+
+//     let sum = num1 + num2
+//    return console.log(sum);
+// }
+
+// operator()
+
+
+// Practice calculator 4
+
+// function getNumber(num){
+//     let oper = document.getElementById("value")
+//     oper += num;
+// }
+// function clearResult(){
+//     let clear = document.getElementById("value")
+//     clear.value = " "
+// }
+
+// function getResult(){
+//     var result = document.getElementById("value");
+//     result.value = eval(result.value);
+// } 
+
+// Question no 4
+
+// function onePlus(num1, num2, operator) {
+//     let result;
+//     if (operator === "+") {
+//         result = num1 + num2;
+//     } else if (operator === "-") {
+//         result = num1 - num2;
+//     } else if (operator === "*") {
+//         result = num1 * num2;
+//     } else if (operator === "/") {
+//         result = num1 / num2
+//     } else {
+//         result = "Invalid operator"
+//     }
+//     document.writeln("Result: " + result,"<br>")
+//     return result;
+// }
+// onePlus(10, 9, "+")
+// onePlus(10, 9, "-")
+// onePlus(10, 9, "*")
+// onePlus(10, 9, "/")
+
+
+// Question no 5
+
+// function square(num){
+//    return num * num
+// }
+// console.log(square(2));
+
+// Question no 6
+
+// function factorail(num) {
+//     let result = 1;
+//     for (let i = num; i >= 1; i--) {
+//         result *= i
+//     }
+//     return result;
+// }
+
+// console.log(factorail(8));
